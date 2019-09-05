b3bc862dff9d1bc845979bd0f3f6b1bf22ec9400
Gensuke Toshiro <toshigem@gmail.com>
2019-09-05 16:05:03 +0900

    Provide a option to respond with normalized error

--- handlers.js
+++ handlers.js
@@ -1,9 +1,11 @@
 const Sentry = require('@sentry/node');
 const { Span } = Sentry;
-const { StatusCodeError } = require('request-promise-core');
+const { StatusCodeError } = require('request-promise-core/errors');
 
 const { SPAN_KEY, LOGGER_KEY, setOptions, options } = require('./options');
 const { loggingHandler, getLogger } = require('./logger');
+
+const stop = () => {};
 
 module.exports = {
 
@@ -28,7 +30,7 @@ module.exports = {
         }
     },
 
-    errorHandler: (err, req, res, next) => {
+    errorHandler: (opts = {}) => (err, req, res, next) => {
         Sentry.withScope(scope => {
             const span = options.httpContext.get(SPAN_KEY) || req.span;
             if (span) {
@@ -39,23 +41,29 @@ module.exports = {
             res.sentry = { eventId };
         });
 
-        loggingHandler.errorLogger(err, req, res, next);
+        if (opts.respondWithError) {
+            if (err instanceof StatusCodeError) {
+                const {
+                    error,
+                    statusCode,
+                    options
+                } = err;
 
-        if (err instanceof StatusCodeError) {
-            const {
-                error,
-                statusCode,
-                options
-            } = err;
-
-            res.status(500).send({
-                message: `request to ${options.method} ${options.uri} failed with status ${statusCode}`,
-                cause: {
-                    status: statusCode,
-                    error
-                }
-            });
+                res.status(500).send({
+                    message: `request ${options.method} ${options.uri} failed with status ${statusCode}`,
+                    cause: {
+                        status: statusCode,
+                        error
+                    }
+                });
+            } else {
+                res.status(500).send({
+                    message: typeof err === 'string' ? err : err.message
+                });
+            }
         }
+
+        loggingHandler.errorLogger(err, req, res, opts.respondWithError ? stop : next);
     }
 
 };
