a42316fd931c80d6c809b084b31645e50b9d44bf
Gensuke Toshiro <toshigem@gmail.com>
2020-04-08 18:35:38 +0900

    Fix trace regexp

--- propagation.js
+++ propagation.js
@@ -4,7 +4,7 @@ const { generate } = require('@opencensus/propagation-stackdriver').v1;
 const TRACEPARENT_REGEXP = new RegExp(
   '^[ \\t]*' + // whitespace
   '([0-9a-f]{32})?' + // trace_id
-  '-?([0-9a-f]{16})?' + // span_id
+  '-?([0-9a-f]+)?' + // span_id
   '-?([01])?' + // sampled
     '[ \\t]*$', // whitespace
 );
@@ -23,7 +23,7 @@ const parse = (trace) => {
     return {
         traceId: matches[1] || '',
         spanId: matches[2] || '',
-        options: isNaN(Number(matches[3])) ? undefined : Number(matches[3])
+        options: isNaN(Number(matches[3])) ? 0 : Number(matches[3])
     };
 };
 
